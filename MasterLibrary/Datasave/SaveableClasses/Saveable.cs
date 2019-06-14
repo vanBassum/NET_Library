90181aeb6a1ede6149a197464418a0464ae6cb29
Bas Visscher <bas@koolecontrols.nl>
2019-06-14 13:50:14 +0200

    added saveable classes

---
+++ MasterLibrary/Datasave/SaveableClasses/Saveable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datasave
+{
+    public interface Saveable
+    {
+        void Save(Stream stream);
+
+        void Load(Stream stream);
+
+    }
+}
