8b172021eaa797aee53e1f8f3899c91f1deda92b
Bas Visscher <bas@koolecontrols.nl>
2019-06-14 13:49:32 +0200

    Added serializers

---
+++ MasterLibrary/Datasave/Serializers/JSON.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datasave.Serializers
+{
+    public class JSON : Serializer
+    {
+        public override T Deserialize<T>(Stream data)
+        {
+            using (StreamReader sr = new StreamReader(data))
+                return JsonConvert.DeserializeObject<T>(sr.ReadToEnd());
+        }
+        public override void Serialize<T>(T obj, Stream stream)
+        {
+            using (StreamWriter sr = new StreamWriter(stream))
+                sr.WriteLine(JsonConvert.SerializeObject(obj, Formatting.Indented));
+        }
+    }
+}
---
+++ MasterLibrary/Datasave/Serializers/Serializer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datasave.Serializers
+{
+    public abstract class Serializer
+    {
+        public abstract void Serialize<T>(T obj, Stream stream);
+        public abstract T Deserialize<T>(Stream stream);
+
+        public void Serialize<T>(T obj, string filename)
+        {
+            using (Stream s = File.Open(filename, FileMode.CreateNew, FileAccess.ReadWrite))
+            {
+                Serialize<T>(obj, s);
+            }
+        }
+
+        public T Deserialize<T>(string filename)
+        {
+            using (Stream s = File.Open(filename, FileMode.Open, FileAccess.ReadWrite))
+            {
+                return Deserialize<T>(s);
+            }
+        }
+
+    }
+}
