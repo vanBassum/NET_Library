d2f0f5ebc6072937e5d0cb89e9782bfa44c54624
Bas Visscher <bas@koolecontrols.nl>
2019-07-12 16:50:52 +0200

    Added some classes and moved all loose stuff to misc

--- MasterLibrary/MasterLibrary/Misc/AutoUpdate.cs
+++ MasterLibrary/MasterLibrary/Misc/AutoUpdate.cs
@@ -7,7 +7,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MasterLibrary
+namespace MasterLibrary.Misc
 {
     public class AutoUpdate
     {
---
+++ MasterLibrary/MasterLibrary/Misc/ControlExtentions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ProjectCopier
+{
+    public static class ControlExtentions
+    {
+        private delegate void SafeCallDelegate(Control c, Predicate<Control> action);
+        public static void InvokeIfRequired(this Control c, Predicate<Control> action)
+        {
+            if (c.InvokeRequired)
+            {
+                c.Invoke(new SafeCallDelegate(InvokeIfRequired), new object[] {c,  action });
+            }
+            else
+            {
+                action(c);
+            }
+        }
+    }
+}
