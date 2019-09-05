bdab981abdc7d16014148f9cffe1f12140c29ab4
Bas Visscher <bas@koolecontrols.nl>
2019-09-05 16:53:27 +0200

    Added function to send username

--- MasterLibrary/MasterLibrary/Ethernet/DataPackages/Frame.cs
+++ MasterLibrary/MasterLibrary/Ethernet/DataPackages/Frame.cs
@@ -17,13 +17,29 @@ namespace MasterLibrary.Ethernet.DataPackages
         public Command CMD => Command.SendID;
 
         public int ID { get; set; }
+
+        public SendID(int id)
+        {
+            ID = id;
+        }
     }
 
+    public class SendUsername : Frame
+    {
+        public Command CMD => Command.SendUsername;
+
+        public string Username { get; set; }
+
+        public SendUsername(string username)
+        {
+            Username = username;
+        }
+    }
 
     public enum Command
     {
         SendID,
         Decline,
-        Accept
+        SendUsername
     }
 }
