e19c28001d2bdfe132c3a1709db22651a76b841d
Bas Visscher <bas@koolecontrols.nl>
2019-09-05 16:12:54 +0200

    Added frame interface as main data command

---
+++ MasterLibrary/MasterLibrary/Ethernet/DataPackages/Frame.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterLibrary.Ethernet.DataPackages
+{
+    public interface Frame
+    {
+        Command CMD { get; }
+
+    }
+
+    public class SendID : Frame
+    {
+        public Command CMD => Command.SendID;
+
+        public int ID { get; set; }
+    }
+
+
+    public enum Command
+    {
+        SendID,
+        Decline,
+        Accept
+    }
+}
