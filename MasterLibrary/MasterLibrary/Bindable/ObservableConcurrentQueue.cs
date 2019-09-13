ac4b262bef882588274319ab567a39f0e736161f
Bas Visscher <bas@koolecontrols.nl>
2019-09-13 08:10:12 +0200

    Threaded queue

---
+++ MasterLibrary/MasterLibrary/Bindable/ObservableConcurrentQueue.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterLibrary.Bindable
+{
+    public class ObservableConcurrentQueue<T> : INotifyCollectionChanged, IEnumerable<T>
+    {
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+        private readonly ConcurrentQueue<T> queue = new ConcurrentQueue<T>();
+
+        public void Enqueue(T item)
+        {
+            queue.Enqueue(item);
+            if (CollectionChanged != null)
+                CollectionChanged(this,
+                    new NotifyCollectionChangedEventArgs(
+                        NotifyCollectionChangedAction.Add, item));
+        }
+
+        public bool TryDequeue(out T itm)
+        {
+            bool okidoki = queue.TryDequeue(out itm);
+            if (CollectionChanged != null)
+                CollectionChanged(this,
+                    new NotifyCollectionChangedEventArgs(
+                        NotifyCollectionChangedAction.Remove, itm));
+            return okidoki;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return queue.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
