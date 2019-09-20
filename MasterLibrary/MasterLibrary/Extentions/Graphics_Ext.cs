343d01ec315bb761ab7480f0e8eac62ce76a79ec
Bas Visscher <bas@koolecontrols.nl>
2019-09-20 12:28:31 +0200

    Minor changes

---
+++ MasterLibrary/MasterLibrary/Extentions/Graphics_Ext.cs
@@ -0,0 +1,40 @@
+﻿using System.Drawing;
+using MasterLibrary.MathX;
+
+namespace MasterLibrary.Extentions
+{
+    public static class Graphics_Ext
+    {
+        public static void DrawCircle(this Graphics g, Pen pen, PointF center, double radius) => g.DrawCircle(pen, center, (float)radius);
+        public static void DrawCircle(this Graphics g, Pen pen, PointF center, float radius)
+        {
+            float x = center.X - radius;
+            float y = center.Y - radius;
+            float width = 2 * radius;
+            float height = 2 * radius;
+            g.DrawEllipse(pen, x, y, width, height);
+        }
+
+
+        public static void DrawCross(this Graphics g, Pen pen, PointF center, float radius)
+        {
+            PointF p1 = new PointF(center.X - radius, center.Y - radius);
+            PointF p2 = new PointF(center.X + radius, center.Y + radius);
+            PointF p3 = new PointF(center.X - radius, center.Y + radius);
+            PointF p4 = new PointF(center.X + radius, center.Y - radius);
+
+            g.DrawLine(pen, p1, p2);
+            g.DrawLine(pen, p3, p4);
+        }
+
+
+
+        public static void DrawV2D(this Graphics g, Pen p, V2D vector, V2D origin)
+        {
+            V2D temp = vector;
+            temp.Y *= -1;
+            g.DrawLine(p, origin, origin + temp);
+        }
+    }
+
+}
--- MasterLibrary/MasterLibrary/Math/V2D.cs
+++ MasterLibrary/MasterLibrary/Math/V2D.cs
@@ -4,15 +4,16 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MasterLibrary.PropertySensitive;
 
 namespace MasterLibrary.MathX
 {
 
-    public class V2D
+    public class V2D : PropertySensitiveExternal
     {
         public static V2D Null { get { return new V2D(0, 0); } }
-        public double X { get; set; }
-        public double Y { get; set; }
+        public double X { get { return GetPar<double>(); } set { SetPar(value); } }
+        public double Y { get { return GetPar<double>(); } set { SetPar(value); } }
         public double Rad { get { return Math.Atan2(Y, X); } set { double l = L; X = l * Math.Cos(value); Y = l * Math.Sin(value); } }
         public double Deg { get { return Rad * 180 / Math.PI; } set { Rad = value * Math.PI / 180; } }
         public double L { get { return Math.Sqrt(Y * Y + X * X); } set { double a = Rad; X = value * Math.Cos(a); Y = value * Math.Sin(a); } }
@@ -30,11 +31,7 @@ namespace MasterLibrary.MathX
             Y = y;
         }
 
-        public void Draw(Graphics g)
-        {
-            V2D dotSize = new V2D(5, 5);
-            g.FillEllipse(Brushes.Black, new RectangleF(this - (dotSize / 2), dotSize));
-        }
+        public V2D Conjugate() => new V2D(X, -Y);
 
         public static V2D operator /(double c, V2D v) => v * c;
         public static V2D operator *(double c, V2D v) => v * c;
@@ -51,6 +48,12 @@ namespace MasterLibrary.MathX
         public static implicit operator V2D(SizeF d) => new V2D(d.Width, d.Height);
         public static implicit operator Size(V2D d) => new Size((int)d.X, (int)d.Y);
         public static implicit operator V2D(Size d) => new V2D(d.Width, d.Height);
-        
+
+
+        public override string ToString()
+        {
+            return X.ToString("0.00") + (Y<0?"":"+") + Y.ToString("0.00") + "i";
+        }
+
     }
 }
