da35700df4ac0db5b534d724524a411f7059da76
Bas Visscher <bas>
2020-03-27 22:37:30 +0100

    Added saveable stuff from net framework to net standard

---
+++ STDLib/Saveable/Saveable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STDLib.Saveable
+{
+    public interface Saveable
+    {
+        void Save(Stream stream);
+        void Load(Stream stream);
+       
+    }
+}
