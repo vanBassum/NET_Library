deab9a55cb52dba1e046a38efebb10f629f17fa8
Bas Visscher <bas.visscher24@gmail.com>
2020-08-03 13:54:24 +0200

    Logger, option to auto open and close logfile

--- STDLib/Misc/Logger.cs
+++ STDLib/Misc/Logger.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Runtime.CompilerServices;
 using System.Text.RegularExpressions;
+using System.Threading;
 
 namespace STDLib.Misc
 {
@@ -12,10 +13,23 @@ namespace STDLib.Misc
 
         private static Logger Instance { get { lock (instance) { return instance; }; } }
 
+        private static string file;
+        private static bool autoOpen;
+        private System.Timers.Timer closeFileTimer = new System.Timers.Timer(1000);
+
+       
+
 
         Logger()
         {
             AppDomain.CurrentDomain.ProcessExit += CurrentDomain_ProcessExit;
+            closeFileTimer.Elapsed += CloseFileTimer_Elapsed;
+        }
+
+        private void CloseFileTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            Instance.closeFileTimer.Stop();
+            Logger.CloseFile();
         }
 
         private void CurrentDomain_ProcessExit(object sender, EventArgs e)
@@ -30,8 +44,17 @@ namespace STDLib.Misc
             Logger.Instance.writer.Dispose();
         }
 
-        public static void OpenFile(string file)
+
+
+        public static void SetFile(string file, bool autoOpen = true)
         {
+            Logger.file = file;
+            Logger.autoOpen = autoOpen;
+        }
+
+        public static void OpenFile()
+        {
+            Console.WriteLine("OpenLOG");
             string path = Path.GetDirectoryName(file);
             if (path != "")
             {
@@ -44,15 +67,23 @@ namespace STDLib.Misc
 
         public static void CloseFile()
         {
-            Logger.Instance.CloseFileInt();
+            Console.WriteLine("CloseLOG");
+            Instance.CloseFileInt();
         }
 
         public static void WriteLine(string message, [CallerMemberName] string memberName = "")
         {
+            if (autoOpen)
+            {
+                Instance.closeFileTimer.Stop();
+                OpenFile();
+            }
             string timestamp = (DateTime.Now.Ticks / 10000).ToString();
 
             Logger.Instance.writer.WriteLine($"{timestamp}, {memberName}, {Regex.Escape(message)}");
-
+            Console.WriteLine($"Log {memberName}: '{message}'");
+            if (autoOpen)
+                Instance.closeFileTimer.Start();
         }
     }
 }
