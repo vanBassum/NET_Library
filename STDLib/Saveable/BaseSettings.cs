1a599735b59181fabbbc2678cf4cf07b488acf7f
Bas Visscher <bas.visscher24@gmail.com>
2020-08-14 17:35:51 +0200

    Added typeconverter to settings getpar when types dont match

--- STDLib/Saveable/BaseSettings.cs
+++ STDLib/Saveable/BaseSettings.cs
@@ -1,6 +1,9 @@
 ﻿using STDLib.Serializers;
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Numerics;
 using System.Runtime.CompilerServices;
 
 namespace STDLib.Saveable
@@ -100,6 +103,8 @@ namespace STDLib.Saveable
 
         }
 
+
+
         /// <summary>
         /// Get the value of a property.
         /// </summary>
@@ -111,9 +116,25 @@ namespace STDLib.Saveable
         {
             lock (fields)
             {
-                if (!fields.ContainsKey(propertyName))
-                    fields[propertyName] = defVal;
-                return (T2)fields[propertyName];
+                T2 val = defVal;
+                try
+                {
+                    if (!fields.ContainsKey(propertyName))
+                        fields[propertyName] = defVal;
+                    
+                    if(val.GetType() == fields[propertyName].GetType())
+                        val = (T2)fields[propertyName];
+                    else
+                        val = (T2)Convert.ChangeType(fields[propertyName], typeof(T2));
+
+
+
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                return val;
             }
         }
 
@@ -126,8 +147,68 @@ namespace STDLib.Saveable
                     Directory.CreateDirectory(dir);
             }
         }
+
+        
     }
 
+
+    /*
+     * 
+     //https://github.com/dotnet/orleans/issues/1269
+        public static class JsonHelper
+        {
+            private static readonly Type[] _specialNumericTypes = { typeof(ulong), typeof(uint), typeof(ushort), typeof(sbyte) };
+
+            /// <summary>
+            /// Converts values that were deserialized from JSON with weak typing (e.g. into <see cref="object"/>) back into
+            /// their strong type, according to the specified target type.
+            /// </summary>
+            /// <param name="value">The value to convert.</param>
+            /// <param name="targetType">The type the value should be converted into.</param>
+            /// <returns>The converted value.</returns>
+            public static object ConvertWeaklyTypedValue(object value, Type targetType)
+            {
+                if (targetType == null)
+                    throw new ArgumentNullException(nameof(targetType));
+
+                if (value == null)
+                    return null;
+
+                if (targetType.IsInstanceOfType(value))
+                    return value;
+
+                var paramType = Nullable.GetUnderlyingType(targetType) ?? targetType;
+
+                if (paramType.IsEnum)
+                {
+                    if (value is string)
+                        return Enum.Parse(paramType, (string)value);
+                    else
+                        return Enum.ToObject(paramType, value);
+                }
+
+                if (paramType == typeof(Guid))
+                {
+                    return Guid.Parse((string)value);
+                }
+
+                if (_specialNumericTypes.Contains(paramType))
+                {
+                    if (value is BigInteger)
+                        return (ulong)(BigInteger)value;
+                    else
+                        return Convert.ChangeType(value, paramType);
+                }
+
+                if (value is long)
+                {
+                    return Convert.ChangeType(value, paramType);
+                }
+
+                throw new ArgumentException($"Cannot convert a value of type {value.GetType()} to {targetType}.");
+            }
+        }
+     */
 
 }
 
