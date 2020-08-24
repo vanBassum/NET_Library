b0d4d1315e1ac21a11c828f85d15926d5cb178ab
Bas Visscher <bas.visscher24@gmail.com>
2020-08-24 22:10:36 +0200

    Added some stuff for the tilemap

---
+++ FRMLib/Kernel32.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace FRMLib
+{
+    public static class Kernel32
+    {
+        public const int PROCESS_ALL_ACCESS = 0x1F0FFF;
+        public const int PROCESS_WM_READ = 0x0010;
+        public const int PROCESS_WM_WRITE = 0x0020;
+        public const int PROCESS_WM_OPERATION = 0x0008;
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [Out] byte[] lpBuffer, int dwSize, out IntPtr lpNumberOfBytesRead);
+
+
+    }
+
+    public class MemoryManager
+    {
+        Process process;
+        IntPtr processHandle;
+
+        #region ProcessAttach
+        
+        public bool attach(string processname)
+        {
+            try
+            {
+                process = Process.GetProcessesByName(processname)[0];
+                processHandle = Kernel32.OpenProcess(Kernel32.PROCESS_WM_READ | Kernel32.PROCESS_WM_WRITE | Kernel32.PROCESS_WM_OPERATION, false, process.Id);
+                return true;
+            }
+            catch
+            {
+                if (process != null)
+                {
+                    process.Close();
+                    process.Dispose();
+                }
+            }
+
+            return false;
+        }
+
+        public bool IsAttached()
+        {
+            if (process != null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool detach()
+        {
+            try
+            {
+                if (process != null)
+                {
+                    process.Close();
+                    process.Dispose();
+                }
+                return true;
+            }
+            catch
+            {
+            }
+
+            return false;
+        }
+
+
+
+        #endregion
+
+        public IntPtr GetProcessBase()
+        {
+            return process.MainModule.BaseAddress;       
+        }
+
+        public IntPtr GetProcessModuleBase(string moduleName)
+        {
+            var mod = process.Modules.Cast<ProcessModule>().Where(p => p.ModuleName == moduleName).FirstOrDefault();
+            if (mod != null)
+                return mod.BaseAddress;
+            return IntPtr.Zero;
+        }
+
+
+        #region Read
+        public byte[] Read_Bytes(IntPtr address, int length)
+        {
+            byte[] buffer = new byte[length];
+            IntPtr bytesRead = IntPtr.Zero;
+            Kernel32.ReadProcessMemory(processHandle, address, buffer, buffer.Length, out bytesRead);
+            return buffer;
+        }
+
+        public IntPtr Read_Address(IntPtr address)
+        {
+            byte[] buffer = Read_Bytes(address, IntPtr.Size);
+            if (IntPtr.Size == 4)
+                return new IntPtr(Read_Int32(address));
+            else
+                return new IntPtr(Read_Int64(address));
+        }
+
+        public Int32 Read_Int32(IntPtr address)
+        {
+            byte[] buffer = Read_Bytes(address, 4);
+            return BitConverter.ToInt32(buffer, 0);
+        }
+
+        public Int64 Read_Int64(IntPtr address)
+        {
+            byte[] buffer = Read_Bytes(address, 8);
+            return BitConverter.ToInt64(buffer, 0);
+        }
+
+        public float Read_Float(IntPtr address)
+        {
+            byte[] buffer = Read_Bytes(address, 4);
+            return BitConverter.ToSingle(buffer, 0);
+        }
+
+        public double Read_Double(IntPtr address)
+        {
+            byte[] buffer = Read_Bytes(address, 4);
+            return BitConverter.ToDouble(buffer, 0);
+        }
+
+        public string Read_String(IntPtr address, int strLength)
+        {
+
+            byte[] buffer = Read_Bytes(address, strLength);
+            char[] chars = new char[strLength / sizeof(char)];
+            System.Buffer.BlockCopy(buffer, 0, chars, 0, buffer.Length);
+            return new string(chars);
+        }
+
+        public byte Read_Byte(IntPtr address)
+        {
+            byte[] buffer = Read_Bytes(address, 1);
+            return buffer[0];
+        }
+        #endregion
+
+
+
+    }
+
+}
--- FRMLib/TileImage/Controls/TileView.cs
+++ FRMLib/TileImage/Controls/TileView.cs
@@ -13,13 +13,18 @@ namespace FRMLib.TileImage
 {
     public partial class TileView : UserControl
     {
+        public event EventHandler<PointD> MouseDownScaled;
+        //public event EventHandler<PointD> MouseUpScaled;
         public Map Map { get; set; }
 
         public PointD Center { get; set; } = new PointD(0,0);
         public int Zoom { get; set; } = 0;
-
+        public event EventHandler<Graphics> OnDrawObjectsScaled;
         PictureBox picBox_Map = new PictureBox();
         PictureBox picBox_Draw = new PictureBox();
+
+        private PointD centerStartDrag;
+        private Point mouseStartDrag;
 
         public TileView()
         {
@@ -36,12 +41,59 @@ namespace FRMLib.TileImage
             picBox_Draw.BackColor = Color.Transparent;
             picBox_Draw.BringToFront();
             picBox_Draw.Paint += PicBox_Draw_Paint;
+
+            picBox_Draw.MouseMove += picBox_MouseMove;
+            picBox_Draw.MouseDown += picBox_MouseDown;
+        }
+
+
+        private void picBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            centerStartDrag = Center;
+            mouseStartDrag = e.Location;
+
+            V2D pt = ((V2D)Center / Map.TileSets[Zoom].Muliplier - ((V2D)this.Size / 2) + (V2D)e.Location) * Map.TileSets[Zoom].Muliplier;
+            MouseDownScaled?.Invoke(this, pt);
+        }
+
+
+        private void picBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(e.Button.HasFlag(MouseButtons.Left))
+            {
+                Center = (V2D)centerStartDrag + ((V2D)mouseStartDrag - (V2D)e.Location) * Map.TileSets[Zoom].Muliplier;
+                picBox_Draw.Refresh();
+                picBox_Map.Refresh();
+            }
         }
 
         private void PicBox_Draw_Paint(object sender, PaintEventArgs e)
         {
-            e.Graphics.DrawLine(Pens.Red, 0, 0, this.Width, this.Height );
+
+            e.Graphics.DrawLine(Pens.Red, 0, 0, this.Width, this.Height);
             e.Graphics.DrawLine(Pens.Red, 0, this.Height, this.Width, 0);
+
+            if (Map != null)
+            {
+                if (Map.TileSets != null)
+                {
+                    if (Map.TileSets.Count > Zoom)
+                    {
+
+                        V2D pt = ((V2D)this.Size / 2) - (V2D)Center / Map.TileSets[Zoom].Muliplier;
+                        e.Graphics.TranslateTransform((float)pt.X, (float)pt.Y);
+                        e.Graphics.ScaleTransform((float)(1.0/Map.TileSets[Zoom].Muliplier), (float)(1.0/Map.TileSets[Zoom].Muliplier));
+                        OnDrawObjectsScaled?.Invoke(this, e.Graphics);
+                    }
+                }
+            }
+
+            
+
+            
+            
+            
+            
         }
 
         private void PicBox_Map_Paint(object sender, PaintEventArgs e)
@@ -51,12 +103,11 @@ namespace FRMLib.TileImage
             {
                 if (Map.TileSets != null)
                 {
-                    RectangleD viewPort = new RectangleD();
-                    viewPort.Size = (V2D)this.Size;
-                    viewPort.Position = (V2D)Center / Map.TileSets[Zoom].Muliplier - ((V2D)viewPort.Size / 2);
-
                     if (Map.TileSets.Count > Zoom)
                     {
+                        RectangleD viewPort = new RectangleD();
+                        viewPort.Size = (V2D)this.Size;
+                        viewPort.Position = (V2D)Center / Map.TileSets[Zoom].Muliplier - ((V2D)viewPort.Size / 2);
                         foreach (Tile tile in Map.TileSets[Zoom].Tiles)
                         {
                             if (RectangleD.Collides(viewPort, tile))
