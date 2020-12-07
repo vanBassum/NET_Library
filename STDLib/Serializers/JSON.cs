8d74656ca03a29cc2cfbfb442bd398265065e9c1
Bas Visscher <bas.visscher24@gmail.com>
2020-12-07 09:34:00 +0100

    Changed JSON around to use public Settings. default no .net names.

--- STDLib/Serializers/JSON.cs
+++ STDLib/Serializers/JSON.cs
@@ -7,75 +7,47 @@ namespace STDLib.Serializers
 {
     public class JSON : Serializer
     {
+        public JsonSerializerSettings Settings { get; set; } = new JsonSerializerSettings
+        {
+            //TypeNameHandling = TypeNameHandling.Objects,
+            //TypeNameAssemblyFormat = FormatterAssemblyStyle.Simple
+        };
+
+
         public override T Deserialize<T>(Stream data)
         {
             using (StreamReader sr = new StreamReader(data))
-            { 
+            {
                 string serial = sr.ReadToEnd();
-
-                return JsonConvert.DeserializeObject<T>(serial, new JsonSerializerSettings
-                {
-                    TypeNameHandling = TypeNameHandling.Objects,
-                    TypeNameAssemblyFormat = FormatterAssemblyStyle.Simple
-                });
-                            }
-                
+                return JsonConvert.DeserializeObject<T>(serial, Settings);
+            }
         }
+
         public override void Serialize<T>(T obj, Stream stream)
         {
             using (StreamWriter sr = new StreamWriter(stream))
             {
-                string serial = JsonConvert.SerializeObject(obj, Formatting.Indented, new JsonSerializerSettings
-                {
-                    TypeNameHandling = TypeNameHandling.Objects,
-                    TypeNameAssemblyFormat = FormatterAssemblyStyle.Simple
-                });
-
+                string serial = JsonConvert.SerializeObject(obj, Formatting.Indented, Settings);
                 sr.WriteLine(serial);
             }
         }
 
         public T Deserialize<T>(byte[] data)
         {
-
             string serial = Encoding.ASCII.GetString(data);
-
-            return JsonConvert.DeserializeObject<T>(serial, new JsonSerializerSettings
-            {
-                TypeNameHandling = TypeNameHandling.Objects,
-                TypeNameAssemblyFormat = FormatterAssemblyStyle.Simple
-            });
-
-            
-
+            return JsonConvert.DeserializeObject<T>(serial, Settings);
         }
+
         public byte[] Serialize<T>(T obj)
         {
-
-            string serial = JsonConvert.SerializeObject(obj, Formatting.Indented, new JsonSerializerSettings
-            {
-                TypeNameHandling = TypeNameHandling.Objects,
-                TypeNameAssemblyFormat = FormatterAssemblyStyle.Simple,
-                Formatting = Formatting.Indented
-            });
-
+            string serial = JsonConvert.SerializeObject(obj, Formatting.Indented, Settings);
             return Encoding.ASCII.GetBytes(serial);
         }
 
-
         public object Deserialize(byte[] data)
         {
-
             string serial = Encoding.ASCII.GetString(data);
-
-            return JsonConvert.DeserializeObject(serial, new JsonSerializerSettings
-            {
-                TypeNameHandling = TypeNameHandling.Objects,
-                TypeNameAssemblyFormat = FormatterAssemblyStyle.Simple
-            });
-
-
-
+            return JsonConvert.DeserializeObject(serial, Settings);
         }
     }
 
