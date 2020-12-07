a32127e6d49cd1c7bf9917d0760a161c6d654421
Bas Visscher <bas.visscher24@gmail.com>
2020-12-07 10:00:47 +0100

    Added generic property editor

---
+++ FRMLib/FormEditObject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FRMLib
+{
+    public partial class FormEditObject : Form
+    {
+        public object Value { get { return propertyGrid1.SelectedObject; } set { propertyGrid1.SelectedObject = value; } }
+
+        public FormEditObject()
+        {
+            InitializeComponent();
+        }
+
+        private void FormEditObject_Load(object sender, EventArgs e)
+        {
+            button1.Click += (s, e2) => DialogResult = DialogResult.OK;
+            button2.Click += (s, e2) => DialogResult = DialogResult.Cancel;
+        }
+    }
+}
