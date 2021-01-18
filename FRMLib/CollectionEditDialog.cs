ff4ccb73a36f6d4f957f271b41c0b51eb06aed59
Bas Visscher <bas.visscher24@gmail.com>
2021-01-18 13:40:00 +0100

    Create object function to collectioneditdialog

--- FRMLib/CollectionEditDialog.cs
+++ FRMLib/CollectionEditDialog.cs
@@ -15,6 +15,7 @@ namespace FRMLib
         public IBindingList DataSource { get => collectionEditControl1.DataSource; set => collectionEditControl1.DataSource = value; }
         public string DisplayMember { get => collectionEditControl1.DisplayMember; set => collectionEditControl1.DisplayMember = value; }
 
+        public Func<object> CreateObject { get => collectionEditControl1.CreateObject; set => collectionEditControl1.CreateObject = value; }
 
         public CollectionEditDialog()
         {
