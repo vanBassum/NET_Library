1ed50e2e046031b9fe685b14c05edd2cc49bc25a
Bas Visscher <bas.visscher24@gmail.com>
2021-02-12 14:53:57 +0100

    merge?

---
+++ STDLib/JBVProtocol/Commands/RoutingInvalid.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace STDLib.JBVProtocol.Commands
+{
+    public class RoutingInvalid : Command
+    {
+        protected override bool IsBroadcast => false;
+        public override UInt32 CommandID => (UInt32)CommandList.RoutingInvalid;
+        public override void Populate(byte[] data)
+        {
+
+        }
+
+        public override byte[] ToArray()
+        {
+            return new byte[0];
+        }
+    }
+
+
+}
---
+++ STDLib/JBVProtocol/Devices/FunctionGenerator/CMD_DrawLine.cs
@@ -0,0 +1,47 @@
+﻿using STDLib.JBVProtocol.Commands;
+using System.Drawing;
+using System;
+using System.Collections.Generic;
+
+namespace STDLib.JBVProtocol.FunctionGenerator
+{
+    public partial class FunctionGenerator
+    {
+        public class CMD_DrawLine : Command
+        {
+            protected override bool IsBroadcast => false;
+            public override UInt32 CommandID => 3;
+
+            public UInt16 X1;
+            public UInt16 Y1;
+            public UInt16 X2;
+            public UInt16 Y2;
+
+            public Color Color { get; set; }
+
+
+            public override void Populate(byte[] data)
+            {
+                throw new NotImplementedException();
+                Color = Color.FromArgb(data[0], data[1], data[2]);
+            }
+
+            public override byte[] ToArray()
+            {
+                List<byte> data = new List<byte>();
+
+                data.AddRange(BitConverter.GetBytes(X1));
+                data.AddRange(BitConverter.GetBytes(Y1));
+                data.AddRange(BitConverter.GetBytes(X2));
+                data.AddRange(BitConverter.GetBytes(Y2));
+                data.Add(Color.R);
+                data.Add(Color.G);
+                data.Add(Color.B);
+                return data.ToArray();
+            }
+        }
+
+
+    }
+
+}
