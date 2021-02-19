21ab61e25d8bd34dad9b7b0abc1fc07ef98ee550
Bas Visscher <bas.visscher24@gmail.com>
2021-02-19 12:08:02 +0100

    Added menuitems to lib

--- FRMLib/Ext.cs
+++ FRMLib/Ext.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.Windows.Forms;
 
 namespace FRMLib
@@ -17,6 +18,71 @@ namespace FRMLib
                 action();
             }
         }
+
+        public static void AddMenuItem(this ToolStrip menu, string menuPath, Action action)
+        {
+            string[] split = menuPath.Split('/');
+
+            ToolStripMenuItem item = null;
+
+            if (menu.Items[split[0]] is ToolStripMenuItem tsi)
+                item = tsi;
+            else
+            {
+                item = new ToolStripMenuItem(split[0]);
+                item.Name = split[0];
+                menu.Items.Add(item);
+            }
+
+            for (int i = 1; i < split.Length; i++)
+            {
+                string name = split[i];
+
+                if (item.DropDownItems[name] is ToolStripMenuItem tsii)
+                    item = tsii;
+                else
+                {
+                    ToolStripMenuItem newItem = new ToolStripMenuItem(name);
+                    newItem.Name = name;
+                    item.DropDownItems.Add(newItem);
+                    item = newItem;
+                }
+
+            }
+
+            if (action != null)
+                item.Click += (a, b) => action.Invoke();
+        }
+
+
+        public static void AddMenuItem(this ToolStripMenuItem menuItem, string menuPath, Action action)
+        {
+            string[] split = menuPath.Split('/');
+
+            ToolStripMenuItem item = menuItem;
+
+            for (int i = 1; i < split.Length; i++)
+            {
+                string name = split[i];
+
+                if (item.DropDownItems[name] is ToolStripMenuItem tsii)
+                    item = tsii;
+                else
+                {
+                    ToolStripMenuItem newItem = new ToolStripMenuItem(name);
+                    newItem.Name = name;
+                    item.DropDownItems.Add(newItem);
+                    item = newItem;
+                }
+
+            }
+
+            if (action != null)
+                item.Click += (a, b) => action.Invoke();
+        }
+
+
+
 
     }
 }
