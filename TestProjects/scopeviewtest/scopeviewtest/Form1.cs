4c108b6780f56daee952a8b789ce526184d70de1
Bas Visscher <bas@koolecontrols.nl>
2021-04-30 10:45:15 +0200

    Added testproject

---
+++ TestProjects/scopeviewtest/scopeviewtest/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FRMLib.Scope;
+
+namespace scopeviewtest
+{
+    public partial class Form1 : Form
+    {
+        ScopeController scope = new ScopeController();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
